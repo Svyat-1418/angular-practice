b21e3f0998802893486394804fa7644c09cbab59
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-09-21 00:06:55 +0300

    creating angular component manually using WebStorm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import {Test1Component} from "./test1/test1.component";
 import { CreatedAuto1Component } from './created-auto1/created-auto1.component';
 import { CreatedAuto2Component } from './created-auto2/created-auto2.component';
+import { Test4Component } from './user/components/test4/test4.component';
+import { Test5Component } from './user/components/test5/test5.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Test1Component,
     CreatedAuto1Component,
-    CreatedAuto2Component
+    CreatedAuto2Component,
+    Test4Component,
+    Test5Component
   ],
   imports: [
     BrowserModule,
